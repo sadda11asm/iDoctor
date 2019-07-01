656ad215bd7afd064ecc743bd4f325ad1d485c6b
porcuon19 <porcuon19@gmail.com>
2019-07-01 13:42:06 +0600

    added chat-ui

---
+++ app/src/main/java/com/example/mppapp/ui/chat/ChatActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mppapp.ui.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mppapp.R
+
+class ChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
