472fa08edf719453080886cddaf1186d0615bdb8
porcuon19 <porcuon19@gmail.com>
2019-07-01 17:51:15 +0600

    added messageAdapter

---
+++ SharedCode/src/commonMain/kotlin/data/entity/Message.kt
@@ -0,0 +1,7 @@
+package org.kotlin.mpp.mobile.data.entity
+
+data class Message(
+    val text: String,
+    val author: String,
+    val created_at: String
+)
---
+++ app/src/main/java/com/example/mppapp/ui/chat/MessageType.kt
@@ -0,0 +1,16 @@
+package com.example.mppapp.ui.chat
+
+
+enum class MessageType {
+
+    SENT_MESSAGE {
+        override fun type() = 0
+
+    },
+    RECEIVED_MESSAGE {
+        override fun type() = 1
+    };
+
+    abstract fun type(): Int
+
+}
