a197411c49ce2c5959649b4ac0ef9b1f9461222d
Saddam <saddam.asmatullayev@nu.edu.kz>
2019-07-17 17:39:06 +0600

    feature was completed

---
+++ SharedCode/src/commonMain/kotlin/data/entity/ChatFull.kt
@@ -0,0 +1,21 @@
+package org.kotlin.mpp.mobile.data.entity
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ChatFull(
+    val title: String?,
+    var updated: String,
+    @SerialName("isAnonymous") var anonymous: Int,
+    var avatar: String?,
+    var lastReadMessage: List<LastReadMessage>?,
+    val messages: MutableList<Message>,
+    var id: Int = -1
+)
+
+@Serializable
+data class LastReadMessage(
+    @SerialName("user_id") var userId: Int,
+    @SerialName("mes_id") var mesId: Int
+)
